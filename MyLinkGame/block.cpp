d7880f06650fedacdedc8c0832c6e18fbf39ee30
hesaidyou <158791443@qq.com>
2018-05-22 08:31:30 +0800

    创建游戏

--- MyLinkGame/block.cpp
+++ MyLinkGame/block.cpp
@@ -1,4 +1,7 @@
 #include <windows.h>
+#include<time.h>
+#include<algorithm>
+#include<iostream>
 #include "block.h"
 //#include "stdafx.h"
 using namespace std;
@@ -16,9 +19,46 @@ Block::~Block() {}
 
 //创建难度为hardRate的游戏，随机排列（未完成）
 void Block::CreatBlocks(int hardRate) {
-	for (int i = 0; i < ROW - 1; i++) {
-		for (int j = 0; j < COLUMN - 1; j++) {
+	cout << "Create The Map" << endl;
+	int b[(ROW - 2)*(COLUMN - 2)] = { 0 };
 
+	//难度1,5种图片
+	if (hardRate == 1) {
+		for (int i = 0; i <(ROW - 2)*(COLUMN - 2); i += 28) {
+			for (int x = 0; x < 28; x++) {
+				b[i + x] = i / 28 + 1;
+			}
+		}
+	}
+
+	//难度2,10种图片
+	if (hardRate == 2) {
+		for (int i = 0; i <(ROW - 2)*(COLUMN - 2); i += 14) {
+			for (int x = 0; x < 14; x++) {
+				b[i + x] = i / 14 + 1;
+			}
+		}
+	}
+
+
+	//难度3,20种图片
+	if (hardRate == 3) {
+		for (int i = 0; i <(ROW - 2)*(COLUMN - 2); i += 7) {
+			for (int x = 0; x < 7; x++) {
+				b[i + x] = i / 7 + 1;
+			}
+		}
+	}
+
+	srand((unsigned)time(NULL));
+	random_shuffle(b, b + (ROW - 2)*(COLUMN - 2));
+	int k = 0;
+	memset(block, 0, sizeof(block));
+
+	for (int i = 1; i < ROW - 1; i++) {
+		for (int j = 1; j < COLUMN - 1; j++) {
+			block[i][j] = b[k];
+			k++;
 		}
 	}
 	block[1][1] = 1;
@@ -29,6 +69,95 @@ void Block::CreatBlocks(int hardRate) {
 
 	block[3][1] = 1;
 	block[3][2] = 2;
+}
+
+void Block::Recreate()
+{
+	cout << "Recreate the map" << endl;
+
+	srand((unsigned)time(NULL));
+
+	int b[(ROW - 2)*(COLUMN - 2)] = { 0 };
+	int i, j;
+	int k = 0;
+	int temp;
+
+	for (int i = 1; i < ROW - 1; i++) {
+		for (int j = 1; j < COLUMN - 1; j++) {
+			b[k] = block[i][j];
+			k++;
+		}
+	}
+	srand((unsigned)time(NULL));
+	for (i = 1; i<(ROW - 2)*(COLUMN - 2); i++) {
+		if (b[i] == 0)
+			continue;
+		j = rand() % (i + 1);
+		k = 2;
+		while (b[j] == 0) {
+			j = rand() % (i + k);
+			k++;
+		}
+		temp = b[i];
+		b[i] = b[j];
+		b[j] = temp;
+	}
+	k = 0;
+	for (int i = 1; i < ROW - 1; i++) {
+		for (int j = 1; j < COLUMN - 1; j++) {
+			block[i][j] = b[k];
+			k++;
+		}
+	}
+	for (int i = 1; i < ROW - 1; i++) {
+		for (int j = 1; j < COLUMN - 1; j++) {
+			if (block[i + 1][j + 1] != 0) {
+				/*
+				删除界面图片信息（未完成）
+				*/
+
+
+			}
+		}
+	}
+	/*
+	然后重新贴图
+
+	*/
+
+
+	/*
+	然后就是CString的操作
+	每选择重排一次，积分扣十分或者多少分
+
+	*/
+}
+
+//测试重拍
+void Block::test_Recreate()
+{
+	for (int i = 1; i < ROW - 1; i++) {
+		for (int j = 1; j < COLUMN - 1; j++) {
+			if (block[i][j] == 5)
+				block[i][j] = 0;
+		}
+	}
+
+
+
+
+}
+
+//测试用
+void Block::print()
+{
+	for (int i = 0; i < ROW; i++) {
+		for (int j = 0; j < COLUMN; j++) {
+			cout << block[i][j] << "  ";
+
+		}
+		cout << endl;
+	}
 }
 
 //提示功能，搜索与之可消除的点，如不存在则提示重排（未完成）
--- MyLinkGame/block.h
+++ MyLinkGame/block.h
@@ -4,8 +4,8 @@
 //连连看消除块类
 
 //行、列
-#define ROW 3+2
-#define COLUMN 3+2
+#define ROW 10+2
+#define COLUMN 14+2
 
 class Block
 {
@@ -14,9 +14,17 @@ public:
 	~Block();
 	//创建难度为hardRate的排列
 	void CreatBlocks(int hardRate);
+	//重排
+	void Recreate();
+	//提示
 	void Search(int row, int column);
+	//判断是否可以消除
 	bool CanRemove(int row1, int column1, int row2, int column2);
 
+	//测试重排
+	void test_Recreate();
+	//测试
+	void print();
 private:
 	int block[ROW][COLUMN];
 };
